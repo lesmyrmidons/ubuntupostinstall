37aaf73251dd848b7885b7d27a9bed14b3a641c7
lesmyrmidons <lesmyrmidons@gmail.com>
2012-10-25 16:39:30 +0200

    update file

--- software/mariadb.sh
+++ software/mariadb.sh
@@ -6,5 +6,9 @@ echo "# MariaDB 5.5 repository list - created 2012-10-25 09:44 UTC
 deb http://mirror2.hs-esslingen.de/mariadb/repo/5.5/ubuntu precise main
 deb-src http://mirror2.hs-esslingen.de/mariadb/repo/5.5/ubuntu precise main" > /etc/apt/sources.list.d/mariadb.list
 
+echo "Ajout du certificat serveur\n"
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
+
 apt-get update
+echo "Installation de MariaDB\n"
 apt-get install mariadb-server-5.5
